b2c6063ffd6345ab01a13944d5c0a5b187bf922d
ohmsmaestro <ohmsmaestro@gmail.com>
2022-12-16 08:49:07 +0000

    added a new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My Name is Omenesa Muhammed");
+        System.out.println("Just starting.....");
     }
 }
